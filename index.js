c9663556c7a7353b3a0dcbbebb605497ca2c3665
JeffreyGrauzer <93175521+JeffreyGrauzer@users.noreply.github.com>
2022-01-18 18:44:52 -0600

    added file structure

---
+++ index.js
@@ -0,0 +1,9 @@
+// require manager,engineer, intern
+// require inquirer const inquirer = require('inquirer')
+// make a function called create Manager which asks a few questions.
+//call another function  to add more employees.
+//Through inquirer you 
+//Based on these choices, create a function for engineer.
+//Consider using switch or if/else for various roles ie if they choose engineer, then run create engineer
+only create 1 manager
+//all functions will be inquirer prompts 
